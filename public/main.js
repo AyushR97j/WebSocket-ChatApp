4c49c31d278274e4f05a44494871cd20723e8599
AyushR97j <ayushraj803110@gmail.com>
2024-01-27 22:44:39 +0530

    UI && Web socket server

---
+++ public/main.js
@@ -0,0 +1,8 @@
+const socket = io()
+
+const clientsTotal = document.getElementById('client-total')
+
+socket.on('clients-total', (data) => {
+    clientsTotal.innerText = `Total Clients: ${data}`
+})
+  
